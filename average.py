447b8dcc7cb4526eb3b3c68d9f5a563068359910
Adelya Fattakhova <AdIFattakhova@stud.kpfu.ru>
2023-05-13 13:19:41 +0300

    Added average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+def calculate_average(numbers):
+    if not numbers:
+        return 0
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+
+# Пример использования
+numbers = [2, 4, 6, 8, 10]
+result = calculate_average(numbers)
+print(f"Среднее значение: {result}")
